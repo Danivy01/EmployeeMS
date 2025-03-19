856e822f3e071c8bcdb9b5f13e303ae6f026cbce
Dani <daniellayvetterimas@gmail.com>
2025-03-19 08:30:28 +0800

    Implemented Position - Model, Controller, and Views

---
+++ EmployeeMS/Controllers/PositionController.cs
@@ -0,0 +1,84 @@
+﻿using EmployeeMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeMS.Controllers
+{
+    public class PositionController : Controller
+    {
+        // GET: Position
+        Positions mod = new Positions();
+        public ActionResult Index()
+        {
+            var list = mod.GetAllRecords();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult InsertRecord()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertRecord(Positions pos)
+        {
+            if (ModelState.IsValid)
+            {
+                mod.InsertRecord(pos);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var item = mod.FindRecord(Id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Positions pos)
+        {
+            if (ModelState.IsValid)
+            {
+                mod.UpdateRecord(pos);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult DeleteRecord(int Id)
+        {
+            var item = mod.FindRecord(Id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteRecord(Positions pos)
+        {
+            if (ModelState.IsValid)
+            {
+                mod.DeleteRecord(pos);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeMS/Models/Positions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMS.Models
+{
+    public class Positions
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<Positions> GetAllRecords()
+        {
+            List<Positions> positions = new List<Positions>();
+            
+            SqlConnection con = new SqlConnection ("SERVER=JOSEITSD\\RAP; DATABASE = dbEMS; INTEGRATED SECURITY = TRUE");
+            con.Open();
+            var cm = new SqlCommand("SELECT * FROM tbl_positions", con);
+            var dt = new DataTable();
+            dt.Load(cm.ExecuteReader());
+            foreach (DataRow r in dt.Rows)
+            {
+                Positions p = new Positions();
+                p.Id = Convert.ToInt32(r["id"]);
+                p.Title = r["title"].ToString();
+                p.Description = r["description"].ToString();
+
+                positions.Add(p);
+            }
+            con.Close();
+            return positions;
+        }
+
+        public void InsertRecord(Positions pos)
+        {
+            SqlConnection con = new SqlConnection("SERVER=JOSEITSD\\RAP;DATABASE=dbEMS;INTEGRATED SECURITY=TRUE");
+            con.Open();
+            var cm = new SqlCommand("INSERT INTO tbl_positions (title, description) VALUES (@title, @description)", con);
+            cm.Parameters.Add(new SqlParameter("@title", pos.Title));
+            cm.Parameters.Add(new SqlParameter("@description", pos.Description));
+            cm.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void UpdateRecord(Positions pos)
+        {
+            SqlConnection con = new SqlConnection("SERVER=JOSEITSD\\RAP;DATABASE=dbEMS;INTEGRATED SECURITY=TRUE");
+            con.Open();
+            var cm = new SqlCommand("UPDATE tbl_positions SET title = @title, description = @description WHERE Id = @Id", con);
+            cm.Parameters.Add(new SqlParameter("@Id", pos.Id));
+            cm.Parameters.Add(new SqlParameter("@title", pos.Title));
+            cm.Parameters.Add(new SqlParameter("@description", pos.Description));
+            cm.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public Positions FindRecord(int Id)
+        {
+            var item = new Positions();
+            SqlConnection con = new SqlConnection("SERVER=JOSEITSD\\RAP;DATABASE=dbEMS;INTEGRATED SECURITY=TRUE");
+            con.Open();
+            var cm = new SqlCommand("SELECT * FROM tbl_positions WHERE Id = @Id", con);
+            cm.Parameters.Add(new SqlParameter("@Id", Id));
+            var dt = new DataTable();
+            dt.Load(cm.ExecuteReader());
+            foreach (DataRow row in dt.Rows)
+            {
+                item = new Positions()
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Title = row["title"].ToString(),
+                    Description = row["description"].ToString()
+                };
+            }
+            con.Close();
+            return item;
+        }
+
+        public void DeleteRecord(Positions pos)
+        {
+            SqlConnection con = new SqlConnection("SERVER=JOSEITSD\\RAP;DATABASE=dbEMS;INTEGRATED SECURITY=TRUE");
+            con.Open();
+            var cm = new SqlCommand("DELETE FROM tbl_positions WHERE Id = @Id", con);
+            cm.Parameters.Add(new SqlParameter("@Id", pos.Id));
+            cm.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
